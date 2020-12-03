08ab4ae1ca2ee702d0632e72fa9ea835f9c83bd1
Roland Strasser <rol1510@users.noreply.github.com>
2020-12-03 23:37:28 +0100

    test script

---
+++ pythonSample/file_test.sh
@@ -0,0 +1,4 @@
+ls -a
+mkdir tmp
+cp pythonSample/test_data/data.txt tmp/
+ls tmp/
--- pythonSample/main.py
+++ pythonSample/main.py
@@ -1,4 +1,12 @@
 import numpy as np
+import os
 
 def func1():
     return np.__version__
+
+
+if __name__ == "__main__":
+    print('file: ', __file__)
+    print('dir:  ', os.path.dirname(__file__))
+
+    
