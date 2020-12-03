1698ceebc889ae292c321c12e06e6e5dc1e90f32
Roland Strasser <rol1510@users.noreply.github.com>
2020-12-03 23:47:21 +0100

    fix 3

--- pythonSample/main.py
+++ pythonSample/main.py
@@ -1,12 +1,7 @@
-import numpy as np
 import os
-
-def func1():
-    return np.__version__
-
 
 if __name__ == "__main__":
     print('file: ', __file__)
     print('dir:  ', os.path.dirname(__file__))
 
-    
+
